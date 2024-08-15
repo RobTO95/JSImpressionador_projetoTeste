ebebd4b913576bd8ca8f7483b88a3326110779df
RobTO95 <roberio.teixeira.oliveira@gmail.com>
2024-08-15 20:08:31 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando github");
 turboLogger("estou aprendendo o fluxo do git");
+turboLogger(
+	"Eu consegui criar uma nova mesnagem sem afetar a versão principal do meu sistema",
+	"red"
+);
